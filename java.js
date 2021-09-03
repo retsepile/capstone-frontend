afc1fba0a2fea7b0691036189750afdc33489754
retsepile <Retshepilekoloko27@gmail.com>
2021-09-03 15:05:31 +0200

    javascript update

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
 function getData() {
-    fetch("https://karabo02.herokuapp.com/client")
+    fetch("https://karabo02.herokuapp.com/client ")
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -11,6 +11,8 @@ function getData() {
   getData();
 
 
+  const div = document.getElementById('bookings');
+  const div = 'https://karabo02.herokuapp.com/location '
 
 
 $(document).ready(function() {
@@ -28,3 +30,17 @@ $(document).ready(function() {
         $('#wrapper').removeClass('toggled');
     });
 });
+
+
+var spread = Spreadsheet.getActiveSpreadsheet();
+var sheet = spread.getSheets()[0];
+var lastRow = sheet.getLastRow();
+var lastColumn = sheet.getLastColumno();
+
+
+function getSubmission(){
+    this.timestamp = sheet.getRange(lastRow, 1).getValue();
+    this.name = sheet.getRange(lastRow,2).getValue();
+    this.reason = sheet.getRange(lastRow, 3).getValue();
+
+}
