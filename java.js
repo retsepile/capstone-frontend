1b90f65bc4cf9222025c7e29de18408b93be247c
retsepile <Retshepilekoloko27@gmail.com>
2021-09-10 14:07:57 +0200

    new changes

--- java.js
+++ java.js
@@ -26,37 +26,21 @@ fetch("https://karabo02.herokuapp.com/location/")
     });
     }
 
-
-  // const div = document.getElementById('bookings');
-  // const div = 'https://karabo02.herokuapp.com/'
-
-
-// $(document).ready(function() {
-//     var open = $('.open-nav'),
-//         close = $('.close'),
-//         overlay = $('.overlay');
-
-//     open.click(function() {
-//         overlay.show();
-//         $('#wrapper').addClass('toggled');
-//     });
-
-//     close.click(function() {
-//         overlay.hide();
-//         $('#wrapper').removeClass('toggled');
-//     });
-// });
-
-
-// var spread = Spreadsheet.getActiveSpreadsheet();
-// var sheet = spread.getSheets()[0];
-// var lastRow = sheet.getLastRow();
-// var lastColumn = sheet.getLastColumno();
-
-
-// function getSubmission(){
-//     this.timestamp = sheet.getRange(lastRow, 1).getValue();
-//     this.name = sheet.getRange(lastRow,2).getValue();
-//     this.reason = sheet.getRange(lastRow, 3).getValue();
-
-// }
+    function renderBookings(bookingsAppointment){
+      let bookingsContainer = document.querySelector("#appointments");
+      bookingsContainer.innerHTML = "";
+      if (bookingsAppointment.lenght > 0){
+        bookingsAppointment.map((bookingsAppointment) => {
+          bookingsContainer.innerHTML += `
+          <div class = "container">
+            <<h2 class = "name_of_continent"> ${bookings[1]}</h2>
+            <p class = "name_of_country"> ${bookings[2]}</p>
+            <p class = "days_of_trip">${bookings[3]}</p>
+            <img class = "image" src="${bookings[4]}"></img>
+            <button onclick="book(${bookings[0]})">Book</button> 
+          
+          </div>`
+        })
+      }
+    }
+  
